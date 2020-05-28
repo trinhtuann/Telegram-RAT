702da06260daf2926aaeadeb83314eba23a59948
Bainky <61879874+Bainky@users.noreply.github.com>
2020-05-28 18:21:39 +0600

    Source

--- Core/Fun/Message.py
+++ Core/Fun/Message.py
@@ -4,4 +4,4 @@ import ctypes
 # Displays a message on the screen
 
 def SendMessageBox(Message):
- ctypes.windll.user32.MessageBoxW(0, Message, u'', 0x40)
+ ctypes.windll.user32.MessageBoxW(0, Message, u'', 0x40) 
