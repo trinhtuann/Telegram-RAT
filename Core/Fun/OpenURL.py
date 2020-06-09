0911550e0502d8b3f6ca56d132e29719331cf78b
Bainky <61879874+Bainky@users.noreply.github.com>
2020-06-09 20:21:06 +0600

    ok

--- Core/Fun/OpenURL.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-# Opens a browser link
-
-def OpenBrowser(URL):
- if not URL.startswith('http'):
-     URL = 'http://' + URL
- return subprocess.Popen('start ' + URL, shell=True)
