3bb5ddb790672bf9adbc9504c013af2d520f9a5c
Bainky <61879874+Bainky@users.noreply.github.com>
2020-06-09 20:21:21 +0600

    ok

--- Core/Fun/Wallpapers.py
+++
@@ -1,7 +0,0 @@
-from ctypes import windll
-
-
-# Sets a photo on the desktop wallpaper
-
-def SetWallpapers(Photo, Directory):
- ctypes.windll.user32.SystemParametersInfoW(20, 0, Directory + Photo.file_path, 0)
